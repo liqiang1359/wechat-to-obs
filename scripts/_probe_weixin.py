989d9b5a336d0422bd3b06545404c1de9b16e9d1
liqiang1359 <liqiang1359@gmail.com>
2026-06-12 15:16:25 +0800

    auto: 自动同步 2026-06-12 15:16:24

--- scripts/_probe_weixin.py
+++ scripts/_probe_weixin.py
@@ -8,25 +8,26 @@ ua = (
   "MicroMessenger/8.0.32 NetType/WIFI Language/zh_CN"
 )
 r = requests.get(url, headers={"User-Agent": ua}, timeout=20, allow_redirects=True)
-html = r.text
-idx = html.find('id="js_content"')
-print("idx", idx)
-if idx >= 0:
-  snippet = html[idx:idx+800]
-  print(snippet)
-# find js_content closing - look for rich_media_content
-for m in re.finditer(r'id="js_content"[^>]*>', html):
-  start = m.end()
-  # count div depth
-  depth = 1
-  i = start
-  while i < len(html) and depth > 0:
-    if html.startswith("<div", i):
-      depth += 1
-    elif html.startswith("</div>", i):
-      depth -= 1
-    i += 1
-  chunk = html[start:i-6]
-  print("depth parse len", len(chunk))
-  print("chunk preview", chunk[:300])
-  break
+h = r.text
+patterns = [
+  r'id=["\']js_content["\']',
+  r"og:image",
+  r"cdn_url",
+  r"picture_page",
+  r"img_item",
+  r"rich_media",
+]
+for pat in patterns:
+  print(pat, len(re.findall(pat, h, re.I)))
+og = re.findall(
+  r'property=["\']og:image["\']\s+content=["\']([^"\']+)["\']',
+  h,
+  re.I,
+)
+print("og", og[:1])
+cdn = re.findall(r'cdn_url:\s*["\']([^"\']+)["\']', h)
+print("cdn", cdn[:2])
+mmbiz = list(dict.fromkeys(re.findall(r"https?://mmbiz\.qpic\.cn/[^\"'<>\s\\]+", h)))
+print("mmbiz unique", len(mmbiz))
+for u in mmbiz[:3]:
+  print(" ", u[:100])
